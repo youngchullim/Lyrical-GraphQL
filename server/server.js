f9887f2527ddcc38ec3c9fc8a383e6c4e0e01c6b
Young Chul Lim <youngchullim95@gmail.com>
2020-01-22 20:25:48 -0800

    Connect to mongolab LyricalGraphQLCluster

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const pw = require('./private');
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb+srv://youngchullim:${pw}@lyrical-graphql-cluster-lb244.mongodb.net/test?retryWrites=true&w=majority`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
