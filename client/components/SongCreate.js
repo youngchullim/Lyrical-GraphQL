6f71e7364b13ad5fa284f702cdcbbfba8d9a5803
Young Chul Lim <youngchullim95@gmail.com>
2020-01-22 21:21:02 -0800

    Add basic SongCreate Component

---
+++ client/components/SongCreate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
