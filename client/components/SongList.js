4681778cf38a12588240f1afd43d66033c83927e
Young Chul Lim <youngchullim95@gmail.com>
2020-01-23 13:07:31 -0800

    Update components and import fetchSongs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import fetchSongs from '../queries/fetchSongs';
 
 class SongList extends Component {
   renderSongs() {
@@ -32,13 +33,4 @@ class SongList extends Component {
   }
 }
 
-const query = gql`
-  {
-    songs {
-      id
-      title
-    }
-  }
-`;
-
-export default graphql(query)(SongList);
+export default graphql(fetchSongs)(SongList);
