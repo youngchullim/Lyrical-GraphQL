9fa40836ab95eda1c151ffa19c412259ab9a1ecb
Young Chul Lim <youngchullim95@gmail.com>
2020-01-24 17:37:28 -0800

    Add a LinkTag to go to each SongDetail Component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -14,7 +14,9 @@ class SongList extends Component {
     return this.props.data.songs.map(({ title, id }) => {
       return ( 
         <li key={id} className="collection-item">
-          {title}
+          <Link to={`songs/${id}`}>
+            {title}
+          </Link>
           <i className="material-icons" onClick={() => this.onSongDelete(id)}>
             delete
           </i>
