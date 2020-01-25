acc658095d522edbafd5611ac3080a3b440627fe
Young Chul Lim <youngchullim95@gmail.com>
2020-01-24 18:08:51 -0800

    Add basic LyricList Component

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
@@ -15,6 +16,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
