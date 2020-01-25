a52b3d213bb6873c58f33421d2a2ae1086f6f050
Young Chul Lim <youngchullim95@gmail.com>
2020-01-24 17:57:45 -0800

    Add createLyricMutation to LyricCreate Component

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        content: this.state.conent,
+        songId: this.props.songId
+      }
+    });
+  }
+  
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
-        <input />
+        <input 
+          value={this.state.content}
+          onChange={e => this.setState({ content: e.target.value })}
+        />
       </form>
     )
   }
 }
+
+const createLyricMutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`;
 
 export default LyricCreate;
