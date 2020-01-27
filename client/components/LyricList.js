90ca66082bfde4e1da10605a496ac4cad5c1bb6e
Young Chul Lim <youngchullim95@gmail.com>
2020-01-27 15:54:40 -0800

    Add likeLyricMutation

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+  onLike(id) {
+    this.props.mutate({ variables: { id } });
+  }
+  
   renderLyrics() {
-    return this.props.lyrics.map(({ content, id}) => {
+    return this.props.lyrics.map(({ content, id, likes}) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <div className="vote-box">
+            <i>{likes}</i>
+            <i className="material-icons" onClick={() => this.onLike(id)}>thumb_up</i>
+          </div>
         </li>
       );
     });
@@ -20,4 +30,14 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const likeLyricMutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      content
+      likes
+    }
+  }
+`;
+
+export default graphql(likeLyricMutation)(LyricList);
